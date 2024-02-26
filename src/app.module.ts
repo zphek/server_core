063b43f7434af93169518c5505360f83ac9b8abc
bernardobaez <bernardbaez.beno@gmail.com>
2024-02-26 17:38:18 -0400

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, ProfilesModule, AuthModule],
+  imports: [UsersModule, ProfilesModule, AuthModule, ProductsModule],
   controllers: [],
   providers: [],
 })
