7913c65e376b99266f448a9ddf1a7ac8b14f3135
bernardobaez <bernardbaez.beno@gmail.com>
2024-02-26 17:39:59 -0400

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { ClientsController } from './clients/clients.controller';
+import { ClientsService } from './clients/clients.service';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [UsersModule, ProfilesModule, AuthModule, ProductsModule],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, ProfilesModule, AuthModule, ProductsModule, ClientsModule],
+  controllers: [ClientsController],
+  providers: [ClientsService],
 })
 export class AppModule {}
