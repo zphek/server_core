423469a60ae48f46592ace166b8da3a929d3cc10
bernardobaez <bernardbaez.beno@gmail.com>
2024-02-29 22:56:53 -0400

    Solo .env

--- db/configuration.ts
+++ db/configuration.ts
@@ -1,7 +1,7 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const sequelize = new Sequelize('ventas', process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize('ventas', 'root', 'bernardo1', {
+  host: 'localhost',
   dialect: 'mysql',
 });
 
