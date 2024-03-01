825a5dd2b1975effce86ae5433351ceb6ce6bd64
bernardobaez <bernardbaez.beno@gmail.com>
2024-02-29 22:46:32 -0400

    SOlucionando mas problemas

--- db/configuration.ts
+++ db/configuration.ts
@@ -1,5 +1,4 @@
 import { Sequelize, DataTypes } from 'sequelize';
-import dotenv from 'dotenv';
 
 const sequelize = new Sequelize('ventas', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '1234', {
   host: process.env.DB_HOST || 'localhost',
