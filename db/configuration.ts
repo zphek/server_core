a27062a9e305a13d263d4135db735bdf190a6a68
bernardobaez <bernardbaez.beno@gmail.com>
2024-02-29 22:50:56 -0400

    Adding a console log to do debugging.

--- db/configuration.ts
+++ db/configuration.ts
@@ -5,4 +5,6 @@ const sequelize = new Sequelize('ventas', process.env.DB_USER || 'root', process
   dialect: 'mysql',
 });
 
+console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST );
+
 export default sequelize;
