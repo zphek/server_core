98ab49ce168748ceae05d0683c54685e03ffe636
bernardobaez <bernardbaez.beno@gmail.com>
2024-03-02 08:13:22 -0400

    Solucion error 2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [UsersService]
 })
 export class AuthModule {}
