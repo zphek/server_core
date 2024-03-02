0dbd9e69171412c2bfb4c403112850df45e683f6
bernardobaez <bernardbaez.beno@gmail.com>
2024-03-02 08:19:29 -0400

    Agregando export al users module, y luego import en auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
-  imports: [UsersService]
+  imports: [UsersModule]
 })
 export class AuthModule {}
