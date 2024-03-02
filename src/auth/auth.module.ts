217506a766aa2ac3eafe49f74920c43c3bcd5da4
bernardobaez <bernardbaez.beno@gmail.com>
2024-03-02 08:14:54 -0400

    ......

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UsersService } from 'src/users/users.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   controllers: [AuthController],
-  providers: [UsersService]
+  imports: [UsersModule]
 })
 export class AuthModule {}
