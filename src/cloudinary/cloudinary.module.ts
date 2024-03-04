6022875508034fc2330b9a83347dda5374e2f4ae
bernardobaez <bernardbaez.beno@gmail.com>
2024-03-04 10:26:07 -0400

    Arreglando errores

--- src/cloudinary/cloudinary.module.ts
+++ src/cloudinary/cloudinary.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryService } from './cloudinary.service';
-import { CloudinaryController } from './cloudinary.controller';
 
 @Module({
-  controllers: [CloudinaryController],
   providers: [CloudinaryService],
 })
 export class CloudinaryModule {}
--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCloudinaryDto } from './dto/create-cloudinary.dto';
-import { UpdateCloudinaryDto } from './dto/update-cloudinary.dto';
+
 
 @Injectable()
 export class CloudinaryService {
-  create(createCloudinaryDto: CreateCloudinaryDto) {
+  create(createCloudinaryDto) {
     return 'This action adds a new cloudinary';
   }
 
@@ -16,7 +15,7 @@ export class CloudinaryService {
     return `This action returns a #${id} cloudinary`;
   }
 
-  update(id: number, updateCloudinaryDto: UpdateCloudinaryDto) {
+  update(id: number, updateCloudinaryDto) {
     return `This action updates a #${id} cloudinary`;
   }
 
