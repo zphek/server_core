62be087a4a692f5a2361e09358f6b82f4f5067e6
bernardobaez <bernardbaez.beno@gmail.com>
2024-03-04 07:20:00 -0400

    Adding api tag to accounts-receivable

--- src/accounts_receivable/accounts_receivable.controller.ts
+++ src/accounts_receivable/accounts_receivable.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Post, Put } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Accounts receivable endpoints:")
 @Controller('accounts-receivable')
 export class AccountsReceivableController {
     @Get("get")
